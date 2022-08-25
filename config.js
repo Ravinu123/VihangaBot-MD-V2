721bb84b10fee8c93630d021c8e7ddeb7b5738c1
Ravinu123 <111894142+Ravinu123@users.noreply.github.com>
2022-08-25 17:08:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,25 +17,25 @@ global.APIKeys = {
 }
 
 //settings
-global.welcomeandgoodbye = 'true' //welcome එක off කරන්න ඕනෙ නම් false දාන්න.
-global.callblock = 'false' //call block on කරන්න ඕනෙ නම් false කියල තියෙන එක මකල true කියල දාන්න.
+global.welcomeandgoodbye = 'true'
+global.callblock = 'false'
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '94712438266' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
-global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
-global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
-global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
+global.owner = ['94712438266',''] //මෙතන number එක.
+global.premium = ['94712438266','94712438266'] //මෙතන number එක.
+global.botname = 'RAVINU BOT INC' //මෙතන නම.
+global.ownername = 'RAVINU MD' //මෙතන නම.
+global.ownername = 'RAVINU BOT' //මෙතන නම.
+global.group1 = 'https://api.whatsapp.com/send?phone=+94712438266' //මෙතන grouplink එක.
+global.group2 = 'https://api.whatsapp.com/send?phone=+94712438266' //මෙතන grouplink එක.
+global.packname = 'RAVINU BOT INC' //මෙතන නම.
+global.cap = '*By RAVINU MD*' //මෙතන නම.
+global.img = 'https://wallpapercave.com/bot-wallpapers' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
+global.botnumber= 'https://api.whatsapp.com/send?phone=+94712438266' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
 global.darahawal = '100' //මෙතන මාරු කරන්න එපා.
 global.besiAwal = '15' //මෙතන මාරු කරන්න එපා.
